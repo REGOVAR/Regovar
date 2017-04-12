38331d36a89b01e11a700c811e3dc2f11f351158
ikit <olivier@labsquare.org>
2017-04-12 16:35:25 +0200

    create database script

---
+++ install/create_all.sql
@@ -0,0 +1,25 @@
+-- 
+-- CREATE ALL - V1.0.0
+--
+
+
+
+CREATE TABLE public.user
+(
+    id serial NOT NULL,
+    login character varying(255) COLLATE pg_catalog.C,
+    password character varying(255) COLLATE pg_catalog.C,
+    email character varying(255) COLLATE pg_catalog.C,
+    firstname character varying(255) COLLATE pg_catalog.C,
+    lastname character varying(255) COLLATE pg_catalog.C,
+    function text COLLATE pg_catalog.C,
+    location text COLLATE pg_catalog.C,
+    last_activity timestamp without time zone,
+    settings text COLLATE pg_catalog.C,
+    authorization text COLLATE pg_catalog.C,
+    CONSTRAINT user_pkey PRIMARY KEY (id),
+    CONSTRAINT user_ukey1 UNIQUE (login),
+    CONSTRAINT user_ukey2 UNIQUE (email)
+);
+ALTER TABLE public.template OWNER TO regovar;
+
