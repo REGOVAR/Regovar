5984c41bd31792150f0d58084915648bf1f343f9
ikit <olivier@labsquare.org>
2018-04-26 13:21:31 +0200

    install script

--- install/init.sh
+++ install/init.sh
@@ -35,15 +35,15 @@ fi
 # Default config value
 # =======================================================================================
 debug="n"
-root_folder="/var/regovar2"
+root_folder="/var/regovar"
 random_key32=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
-omim_key="piout"
-public_host="localhost2"
-db_user="regovar2"
-db_pwd="regovar2"
-db_name="regovar2"
-regovar_pg_port=20051
-regovar_app_port=20050
+omim_key=""
+public_host="localhost"
+db_user="regovar"
+db_pwd="regovar"
+db_name="regovar"
+regovar_pg_port=8501
+regovar_app_port=8500
 
 
 # =======================================================================================
@@ -80,7 +80,7 @@ if [ -n "$answer" ]; then
     omim_key=$answer
 fi
 
-echo -n "5) Public hostname (default: $public_host):"
+echo -n "5) Public hostname (default: $public_host): "
 read answer
 if [ -n "$answer" ]; then
     public_host=$answer
@@ -138,13 +138,13 @@ echo -e " - ${WHITE}APP_PORT:${NC}\t\t$regovar_app_port"
 
 
 
-echo -e "\nSetting up Regovar:\n======================================================================================="
 # =======================================================================================
 # Preparing files repository on HOST
 # =======================================================================================
+echo -e "\nGenerating config files:\n======================================================================================="
 if test "$(ls -A "$root_folder")"; then
-    echo -e "  ${YELLOW}Warning:${NC} The directory $root_folder already exists."
-    echo -e -n "           Move all its content to $root_folder-bak and continue (yes) ? Or cancel installation (no) [y|n]: "
+    echo -e "${YELLOW}Warning:${NC} The directory $root_folder already exists."
+    echo -e -n "         Move all its content to $root_folder-bak and continue (yes) ? Or cancel installation (no) [y|n]: "
     read answer
     if [ "y" == "$answer" ]; then
         mv -f $root_folder $root_folder-bak
@@ -153,68 +153,80 @@ if test "$(ls -A "$root_folder")"; then
         exit 2
     fi
 fi
-mkdir -p $root_folder/{app,config,cache,downloads,files,pipelines,jobs,pgdata,databases/hg19,databases/hg38}
-echo -e "  ${GREEN}Done:${NC} Regovar folders created on HOST: $root_folder"
+mkdir -p ${root_folder}/{app,config,cache,downloads,files,pipelines,jobs,pgdata,databases/hg19,databases/hg38}
+echo -e "${GREEN}Done:${NC} Regovar folders created on HOST: $root_folder"
 
 
 
 # =======================================================================================
 # Generating: regovar_app Dockerfile
 # =======================================================================================
-echo -e -n "  ${YELLOW}In progress${NC}: Generating Dockerfile for regovar_app docker image"
+echo -e -n "${YELLOW}In progress${NC}: Generating Dockerfile for regovar_app docker image"
 cp config/dockerfile.default $root_folder/config/Dockerfile
 # Nothing to do ...
-echo -e "\r  ${GREEN}Done${NC}: Generating Dockerfile for regovar_app docker image"
+echo -e "\r${GREEN}Done${NC}: Generating Dockerfile for regovar_app docker image"
 
 
 
 # =======================================================================================
 # Generating: docker-compose
 # =======================================================================================
-echo -e -n "  ${YELLOW}In progress${NC}: Generating docker-compose file"
+echo -e -n "${YELLOW}In progress${NC}: Generating docker-compose file"
 cp config/docker-compose.default $root_folder/config/regovar.yml
-# (      - /var/regovar)/pgdata...    => (      - $root_folder)/pgdata...
-# (      - /var/regovar)/databases... => (      - $root_folder)/databases...
-# (      - /var/regovar):...          => (      - $root_folder):...
-sed -i 's/^\(\s*\/var\/regovar\)\(\/pgdata.*\)/$(root_folder)\2/' $root_folder/config/regovar.yml
-sed -i 's/^\(\s*\/var\/regovar\)\(\/databases.*\)/$(root_folder)\2/' $root_folder/config/regovar.yml
-sed -i 's/^\(\s*\/var\/regovar\)\(\:.*\)/$(root_folder)\2/' $root_folder/config/regovar.yml
-echo -e "\r  ${GREEN}Done${NC}: Generating docker-compose file"
+# conver / into \/
+sed_root_folder=${root_folder//\//\\/}
+# replace each first occurence of /var/regovar (HOST paths) by provided path
+sed -i s/"\/var\/regovar"/"$sed_root_folder"/ $root_folder/config/regovar.yml
 
-
+sed -i s/"8501"/"$regovar_pg_port"/ $root_folder/config/regovar.yml
+sed -i s/"8500"/"$regovar_app_port"/ $root_folder/config/regovar.yml
+sed -i s/"^\(\s*POSTGRES_USER\s*=\s*\)\(.*\)"/"\1$db_user"/ $root_folder/config/regovar.yml
+sed -i s/"^\(\s*POSTGRES_PASSWORD\s*=\s*\)\(.*\)"/"\1$db_pwd"/ $root_folder/config/regovar.yml
+sed -i s/"^\(\s*POSTGRES_DB\s*=\s*\)\(.*\)"/"\1$db_name"/ $root_folder/config/regovar.yml
+echo -e "\r${GREEN}Done${NC}: Generating docker-compose file"
 
 
 
 # =======================================================================================
 # Generating: regovar config.py
 # =======================================================================================
-echo -e -n "  ${YELLOW}In progress${NC}: Generating regovar app python config file"
+echo -e -n "${YELLOW}In progress${NC}: Generating regovar app python config file"
 cp config/config.default $root_folder/config/config.py
-sed -i 's/^\(\s*DEBUG\s*=\s*\)(.*\)/\1$(debug)/' $root_folder/config/config.py
-sed -i 's/^\(\s*HOST_P\s*=\s*"\)\([^"]\+\)\(".*\)/\1$(public_host)\3/' $root_folder/config/config.py
-sed -i 's/^\(\s*PRIVATE_KEY32\s*=\s*"\)\([^"]\+\)\(".*\)/\1$(random_key32)\3/' $root_folder/config/config.py
-sed -i 's/^\(\s*OMIM_API_KEY\s*=\s*"\)\([^"]\+\)\(".*\)/\1$(omim_key)\3/' $root_folder/config/config.py
-sed -i 's/^\(\s*DATABASE_USER\s*=\s*"\)\([^"]\+\)\(".*\)/\1$(db_user)\3/' $root_folder/config/config.py
-sed -i 's/^\(\s*DATABASE_PWD\s*=\s*"\)\([^"]\+\)\(".*\)/\1$(db_pwd)\3/' $root_folder/config/config.py
-sed -i 's/^\(\s*DATABASE_NAME\s*=\s*"\)\([^"]\+\)\(".*\)/\1$(db_name)\3/' $root_folder/config/config.py
-echo -e "\r  ${GREEN}Done${NC}: Generating regovar app python config file"
-
-
-
-
-
+# conver / into \/ and . into \.
+public_host=${public_host//\//\\/}
+public_host=${public_host//\./\\.}
+sed -i s/"^\(\s*DEBUG\s*=\s*\)\(.*\)"/"\1$debug"/ $root_folder/config/config.py
+sed -i s/"^\(\s*HOST_P\s*=\s*\"\)\(.*\)\(\".*\)"/"\1$public_host\3"/ $root_folder/config/config.py
+sed -i s/"^\(\s*PRIVATE_KEY32\s*=\s*\"\)\(.*\)\(\".*\)"/"\1$random_key32\3"/ $root_folder/config/config.py
+sed -i s/"^\(\s*OMIM_API_KEY\s*=\s*\"\)\(.*\)\(\".*\)"/"\1$omim_key\3"/ $root_folder/config/config.py
+sed -i s/"^\(\s*DATABASE_USER\s*=\s*\"\)\(.*\)\(\".*\)"/"\1$db_user\3"/ $root_folder/config/config.py
+sed -i s/"^\(\s*DATABASE_PWD\s*=\s*\"\)\(.*\)\(\".*\)"/"\1$db_pwd\3"/ $root_folder/config/config.py
+sed -i s/"^\(\s*DATABASE_NAME\s*=\s*\"\)\(.*\)\(\".*\)"/"\1$db_name\3"/ $root_folder/config/config.py
+echo -e "\r${GREEN}Done${NC}: Generating regovar app python config file"
 
 
 
 # =======================================================================================
 # Generating: nginx sites-availables file
 # =======================================================================================
-echo -e -n "  ${YELLOW}In progress${NC}: Generating nginx config file"
+echo -e -n "${YELLOW}In progress${NC}: Generating nginx config file"
 cp config/nginx.default $root_folder/config/regovar
-echo -e -n "  ${GREEN}Done${NC}: Generating nginx config file"
+sed -i s/"8500"/"$regovar_app_port"/ $root_folder/config/regovar
+sed -i s/"test\.regovar\.org"/"$public_host"/ $root_folder/config/regovar
+echo -e "\r${GREEN}Done${NC}: Generating nginx config file"
 
-# regovar_pg_port=10051
-# regovar_app_port=10050
+# apply config
+if test "$(ls -A "/etc/nginx/sites-available/regovar")"; then
+    mv -f /etc/nginx/sites-available/regovar /etc/nginx/sites-available/regovar.bak
+fi
+mv -f $root_folder/config/regovar /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/regovar $root_folder/config/nginx
+/etc/init.d/nginx restart
+
+
+
+# TODO: qu'est-ce qu'on fait ? probleme du sudo
+chown olivier:olivier -R $root_folder
 
 
 
@@ -222,6 +234,15 @@ echo -e -n "  ${GREEN}Done${NC}: Generating nginx config file"
 # =======================================================================================
 # Build regovar container
 # =======================================================================================
+echo -e "\nBuilding docker containers:\n======================================================================================="
+cp *.sql $root_folder/config/
+cd $root_folder/config
+docker rm --force regovar_pg
+docker rm --force regovar_app
+docker rmi --force docker_regovar_app:latest
+docker-compose -f regovar.yml build -d
+echo -e "${GREEN}Done${NC}: Docker containers ready to use"
+rm *.sql 
 
 
 
@@ -229,6 +250,19 @@ echo -e -n "  ${GREEN}Done${NC}: Generating nginx config file"
 # =======================================================================================
 # Install regovar application and database
 # =======================================================================================
+echo -e "\nDatabase creation:\n======================================================================================="
+docker exec regovar_pg psql -U postgres -c "DROP DATABASE IF EXISTS $db_name"
+docker exec regovar_pg psql -U postgres -c "CREATE DATABASE $db_name OWNER $db_user"
+docker exec regovar_pg psql -U postgres -c "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\""
+docker exec regovar_pg psql -U $db_user -d $db_name -f /var/regovar/install/create_all.sql
+docker exec regovar_pg psql -U $db_user -d $db_name -f /var/regovar/install/install_hg19.sql
+docker exec regovar_pg psql -U $db_user -d $db_name -f /var/regovar/install/install_hg38.sql
+echo -e "${GREEN}Done${NC}: Database created"
+
+echo -e "\nRegovar application installation:\n======================================================================================="
+docker exec regovar_app make install_hpo
+echo -e "${GREEN}Done${NC}: Regovar application installation"
+
 
 
 
@@ -239,7 +273,7 @@ echo -e -n "  ${GREEN}Done${NC}: Generating nginx config file"
 
 
 echo -e "\nFinnish ! "
-chown olivier:olivier -R $root_folder
+
 
 
                                    
