1a2756fc4699be43eb8ce11be13ef5232b0e9e55
Oodnadatta <asdeno@hotmail.fr>
2018-06-20 17:50:47 +0200

    Complete paths for Waffle

---
+++ annotations/annotationGRCh37_waffle.sh
@@ -0,0 +1,63 @@
+#! /bin/sh
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $(basename $0) input.vcf output.vcf" >&2
+    exit 1
+fi
+
+input="$(readlink -f $1)"
+output="$(readlink -f $2)"
+
+# VEP
+/opt/ensembl/91/vep/homo_sapiens_merged/91_GRCh37/  \
+    --fork 30 \
+    --buffer_size 200000 \
+    --cache --dir /opt/ensembl/vep/91/homo_sapiens_merged/91_GRCh37/ --offline \
+    --fasta /home/asdp/.local/share/lxc/veptwo/rootfs/opt/b37/human_g1k_v37_decoy.fasta.gz \
+    --db_version 91 \
+    --species homo_sapiens \
+    --assembly GRCh37 \
+    --no_escape \
+    --vcf_info_field CSQ \
+    --terms ensembl \
+    --hgvs \
+    --hgvsg \
+    --shift_hgvs 1 \
+    --transcript_version \
+    --protein \
+    --symbol \
+    --ccds \
+    --uniprot \
+    --appris \
+    --canonical \
+    --biotype \
+    --check_existing \
+    --af \
+    --max_af \
+    --af_1kg \
+    --af_esp \
+    --af_gnomad \
+    --af_exac \
+    --pubmed \
+    --all_refseq \
+    --exclude_predicted \
+    --allow_non_variant \
+    --format vcf \
+    --vcf \
+    --compress_output bgzip \
+    --no_stats \
+    -i "$input" \
+    -o "$output"
+
+# SnpEff
+#java -jar -Xmx16g ~/tools/snpEff/snpEff.jar \
+#     eff GRCh37.75 \
+#     -t \
+#     -i vcf \
+#     -o vcf \
+#     -nodownload \
+#     -sequenceOntology \
+#     -lof \
+#     -noStats \
+#     "$input" \
+#     j> "$output"
